4d6f53d0752dc40e216b7a640d997247e04f3ca7
SannaAb <sannaabrahamsson@gmail.com>
2019-10-02 13:49:50 +0200

    u

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/SannaAb/Pseudogenes",
-     packages=["sys", "os", "pandas", "itertools", "csv", "re", "subprocess", "glob", "shutil", "pysam", "collections", "time","argparse", "logging", "psutil"],
+     packages=setuptools.find_packages()
      classifiers=[
          "Programming Language :: Python :: 2",
          
