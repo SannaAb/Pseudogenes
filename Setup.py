aaacf9ad5eae0d9ba052dff8c6fd09ffd4118236
SannaAb <sannaabrahamsson@gmail.com>
2019-10-02 13:44:34 +0200

    setup added

---
+++ Setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='Ppsy',  
+     version='0.1',
+     scripts=['Ppsy.py'] ,
+     author="Sanna Abrahamsson",
+     author_email="sannaabrahamsson@gmail.com",
+     description="Script to detect Processed Pseudogenes",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/SannaAb/Pseudogenes",
+     packages=["sys", "os", "pandas", "itertools", "csv", "re", "subprocess", "glob", "shutil", "pysam", "collections", "time","argparse", "logging", "psutil"],
+     classifiers=[
+         "Programming Language :: Python :: 2",
+         
+     ],
+ )
