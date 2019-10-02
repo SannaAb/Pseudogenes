fcf61cb58235da565d07a7f8d4c666e8d29200e5
SannaAb <sannaabrahamsson@gmail.com>
2019-10-02 14:31:43 +0200

    u

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='Ppsy',  
      version='0.1',
-     scripts=['Ppsy.py'] ,
+     scripts=['scripts/Ppsy.py'] ,
      author="Sanna Abrahamsson",
      author_email="sannaabrahamsson@gmail.com",
      description="Script to detect Processed Pseudogenes",
@@ -12,6 +12,8 @@ setuptools.setup(
    long_description_content_type="text/markdown",
      url="https://github.com/SannaAb/Pseudogenes",
      packages=setuptools.find_packages(),
+    package_data={'Ppsy': ['README.md', 'HG19_databases/Exon_coord_hg19_refgene.bed',"HG19_databases/Gene_coord_hg19_refgene.bed","HG19_databases/KnownProcessedPseudogenes_Homo_sapiens.GRCh37.75_CHR.bed","HG19_databases/Ppsy_Config.txt"]
+                   },
      classifiers=[
          "Programming Language :: Python :: 2", 
          
