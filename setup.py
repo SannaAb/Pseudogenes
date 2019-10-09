d33cdf3f231bbdd1901a1f5b8c32e04bff1b0c7a
SannaAb <sannaabrahamsson@gmail.com>
2019-10-09 09:38:45 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/py 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name='Ppsy',  
     version='0.1',
-    scripts=['Ppsy.py'] ,
+    scripts=['Scripts/Ppsy.py','Scripts/MakePPsyReport.py'] ,
     author="Sanna Abrahamsson",
     author_email="sannaabrahamsson@gmail.com",
     description="Script to detect Processed Pseudogenes",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/SannaAb/Pseudogenes",
     packages=setuptools.find_packages(),
