90a9e9a50bada3ed824f38545b501b1c5e519f70
SannaAb <sannaabrahamsson@gmail.com>
2021-06-07 09:51:45 +0200

    updated scripts

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='Ppsy',  
     version='0.1',
-    scripts=['Scripts/Ppsy.py','Scripts/MakePPsyReport_html.py', 'Scripts/MakePPsyReport_excel.py', 'Scripts/CreateCircosImageFromPpsyOut.py', 'Scripts/Ppsy_Py3.py', 'Scripts/MakePPsyReport_html_Py3.py'] ,
+    scripts=['Scripts/Ppsy.py','Scripts/MakePPsyReport_html.py', 'Scripts/MakePPsyReport_excel.py', 'Scripts/CreateCircosImageFromPpsyOut.py', 'Scripts/Ppsy.py', 'Scripts/MakePPsyReport_html.py', 'Scripts/CreateiGVImageFromPpsyOut.py'] ,
     author="Sanna Abrahamsson",
     author_email="sannaabrahamsson@gmail.com",
     description="Script to detect Processed Pseudogenes",
@@ -13,12 +13,12 @@ setuptools.setup(
     url="https://github.com/SannaAb/Pseudogenes",
     packages=setuptools.find_packages(),
     install_requires=['Cython','pandas','pysam','psutil','XlsxWriter'],
-    package_data={'Ppsy': ['README.md', 'HG19_databases/Exon_coord_hg19_refgene.bed',"HG19_databases/Gene_coord_hg19_refgene.bed","HG19_databases/KnownProcessedPseudogenes_Homo_sapiens.GRCh37.75_CHR.bed"]
+    package_data={'Ppsy': ['README.md', 'HG19_databases/Exon_coord_hg19_refgene.bed',"HG19_databases/Gene_coord_hg19_refgene.bed","HG19_databases/KnownProcessedPseudogenes_Homo_sapiens.GRCh37.75_CHR.bed", "TestDataset/CBX3_chr15_40854180_insert.r1.fq","TestDataset/CBX3_chr15_40854180_insert.r2.fq"]
                    },
-    data_files= [('HG19_databases',['HG19_databases/Exon_coord_hg19_refgene.bed','HG19_databases/Gene_coord_hg19_refgene.bed','HG19_databases/KnownProcessedPseudogenes_Homo_sapiens.GRCh37.75_CHR.bed'])],
+    data_files= [('HG19_databases',['HG19_databases/Exon_coord_hg19_refgene.bed','HG19_databases/Gene_coord_hg19_refgene.bed','HG19_databases/KnownProcessedPseudogenes_Homo_sapiens.GRCh37.75_CHR.bed', 'TestDataset/CBX3_chr15_40854180_insert.r1.fq','TestDataset/CBX3_chr15_40854180_insert.r2.fq'])],
     include_package_data=True,
     classifiers=[
-         "Programming Language :: Python :: 2", 
+         "Programming Language :: Python :: 3", 
          
      ],
  )
